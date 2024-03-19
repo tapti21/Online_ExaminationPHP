acb711c2db77541d23ba7ec62b8f7f56ad148209
tapti21 <traptishakya21@gmail.com>
2024-03-19 14:27:09 +0530

    first commit

---
+++ admin/inc/footer.php
@@ -0,0 +1,7 @@
+ </section>
+<section class="footeroption">
+		<h2><?php echo "&copy; Submitted By Trapti"; ?></h2>
+	</section>
+</div>
+</body>
+</html>
---
+++ inc/footer.php
@@ -0,0 +1,18 @@
+ </section>
+<section class="footeroption">
+		<h2>
+			<span>ADIT Exam</span>
+			<script type="text/javascript">
+				var d = new Date();
+				document.write(d.getFullYear());
+			</script>
+			<span>
+				<!-- <a target="_blank" href="https://www.facebook.com/promahmudul"> -->
+				
+				</a>
+			</span>
+		</h2>
+	</section>
+</div>
+</body>
+</html>
